6f60077402f00131e93627015b7ea7db21ee5ec0
Peter Chege <peterkagure@gmail.com>
2026-01-11 22:37:55 +0300

    Initial Commit

---
+++ TutorialMovieApp/TutorialMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialMovieAppApp.swift
+//  TutorialMovieApp
+//
+//  Created by Peter Chege on 11/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TutorialMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
